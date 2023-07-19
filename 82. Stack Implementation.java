8fe4729c45729ceda3b5bd1c6bbdb7029aa5035a
Harsh Gupta <97297407+609harsh@users.noreply.github.com>
2023-07-19 19:00:16 +0530

    19-07

---
+++ 82. Stack Implementation.java
@@ -0,0 +1,39 @@
+public class Solution{
+    static class Stack {
+        int size;
+        int curr;
+        int a[];
+        Stack(int capacity) {
+            this.size=capacity;
+            this.curr=0;
+            a=new int[capacity];
+            // Write your code here.
+        }
+        public void push(int num) {
+            if(this.curr==this.size)return;
+            a[this.curr]=num;
+            this.curr+=1;
+            // Write your code here.
+        }
+        public int pop() {
+            // Write your code here.
+            if(this.curr<=0)return -1;
+            int num=a[this.curr-1];
+            this.curr-=1;
+            return num;
+        }
+        public int top() {
+            if(this.curr<=0)return -1;
+            return a[this.curr-1];
+            // Write your code here.
+        }
+        public int isEmpty() {
+            return this.curr==0?1:0;
+            // Write your code here.
+        }
+        public int isFull() {
+            return this.curr==this.size?1:0;
+            // Write your code here.
+        }
+    }
+}
